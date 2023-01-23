69e876ead53a5677847912a5cafa0aa8b7e38806
Parallels <parallels@debian-gnu-linux-11.localdomain>
2023-01-24 00:47:17 +0100

    starting out with nginx but some error popped up with chmod in mariadb dockerfile

---
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# sleep until mariadb is up and running. obsolete when health check is implemented
+sleep 3s
+
+echo "entrypoint: Configuring nginx..."
+
+
+
+
+echo "entypoint: nginx config complete."
+
