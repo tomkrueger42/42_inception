29a8777d7ee3f073e6624ab6c777ce0b6a5d06e8
Tom Krüger <tom@joloto.de>
2023-02-03 19:01:16 +0100

    made everything debian:buster instead of alpine

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,43 +1,69 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "entrypoint: Configiring mariaDB..."
+if [ ! -d "/var/lib/mysql/wordpress_data" ]; then
+echo "before dir does not exist"
 
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
+service mysql start
+sleep 1
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	chown -R mysql:mysql /var/lib/mysql
-
-	# init database
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
-
-	/usr/bin/mysqld --user=mysql --bootstrap << EOF
-
-USE mysql;
-FLUSH PRIVILEGES;
-
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM	mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-
-CREATE DATABASE $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-
-FLUSH PRIVILEGES;
+mysql -u root << EOF
+CREATE DATABASE wordpress_data;
+CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
 EOF
 
+service mysql stop 
+sleep 1
+echo "done dir does not exist"
+
+else
+echo "before dir does exist"
 fi
 
-# allow remote connections
-sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/my.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/my.cnf
+mysqld_safe
 
-echo "entrypoint: mariaDB config complete."
 
-exec /usr/bin/mysqld --user=mysql --console
+# #!/bin/sh
+
+# echo "entrypoint: Configiring mariaDB..."
+
+# if [ ! -d "/run/mysqld" ]; then
+# 	mkdir -p /run/mysqld
+# 	chown -R mysql:mysql /run/mysqld
+# fi
+
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
+# 	chown -R mysql:mysql /var/lib/mysql
+
+# 	# init database
+# 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+
+# 	/usr/bin/mysqld --user=mysql --bootstrap << EOF
+
+# USE mysql;
+# FLUSH PRIVILEGES;
+
+# DROP DATABASE test;
+# DELETE FROM mysql.db WHERE Db='test';
+# DELETE FROM	mysql.user WHERE User='';
+# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+
+# CREATE DATABASE $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
+# CREATE USER '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
+# GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+
+# FLUSH PRIVILEGES;
+# EOF
+
+# fi
+
+# # allow remote connections
+# sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/my.cnf
+# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/my.cnf
+
+# echo "entrypoint: mariaDB config complete."
+
+# exec /usr/bin/mysqld --user=mysql --console
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-
+#!/bin/bash
+sleep 5
+cd /var/www/html
+wp core download --allow-root
+cp /wp-config.php /var/www/html/
+wp core install --allow-root --url=$HODO --title=42Inception42 --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=info@example.com
+mkdir /run/php
+cd -
+php-fpm7.3 -F
